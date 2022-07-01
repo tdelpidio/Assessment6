9adb0a4ed87d0907dbbcd8747879b43bf630ef18
Tessa <106116613+tdelpidio@users.noreply.github.com>
2022-07-01 17:13:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ var rollbar = new Rollbar({
 
 rollbar.log("Hello world!");
 
-app.get('/', (reg,res) => {
+app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-app.get('/styles', (reg,res) => {
+app.get('/styles', (req,res) => {
     res.sendFile(path.join(__dirname, 'public/index.css'))
 })
 
-app.get('/js', (reg,res) => {
+app.get('/js', (req,res) => {
     res.sendFile(path.join(__dirname, 'public/index.js'))
 })
 
