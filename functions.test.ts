c1563ec47f61f256c8e6fcc25d403de3e283aeac
Tessa <106116613+tdelpidio@users.noreply.github.com>
2022-07-01 18:12:31 -0400

    Update functions.test.ts

--- functions.test.ts
+++ functions.test.ts
@@ -4,13 +4,11 @@ let testData = [1, 2, 3, 4, 5]
 
 describe('shuffleArray should', () => {
     test('array length', () => {
-        let formattedArray = shuffleArray(testData)
-        expect(formattedArray.length).toBe(formattedArray.length)
+        expect(shuffleArray(testData).length).toBe(testData.length)
       });
 
-      test('type', () => {
-        let formattedArray = shuffleArray(testData)
-        expect(typeof formattedArray).toBe("array")
+      test('output type', () => {
+        expect(Array.isArray(shuffleArray(testData))).toBe(true)
       })
 
 })
